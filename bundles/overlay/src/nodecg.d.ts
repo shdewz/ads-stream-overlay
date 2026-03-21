561df8cc64e8d3279b95bdcc69db6c27d7fc63ce
shdewz <shdewz@gmail.com>
2026-03-21 06:24:15 +0200

    Fix line endings

--- bundles/overlay/src/nodecg.d.ts
+++ bundles/overlay/src/nodecg.d.ts
@@ -1,6 +1,3 @@
-// Minimal ambient declaration for the nodecg global available in graphics.
-// The full NodeCG API is more extensive; only what this bundle uses is typed.
-
 interface NodeCGReplicant<T> {
   value: T | undefined;
   on(event: 'change', handler: (newValue: T, oldValue: T | undefined) => void): this;
